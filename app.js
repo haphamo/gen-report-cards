389e8ef1b6fa7c3b532ec4aef841732fb76a101f
haphamo <hapham0392@gmail.com>
2020-04-28 13:00:54 -0400

    created callback function to read data from each csv file

--- app.js
+++ app.js
@@ -2,22 +2,19 @@ const fs              = require('fs');
 const csv             = require('csv-parser')
 const args            = process.argv.slice(2)
 
-const coursesFilePath = './data/courses.csv'
+// create a function that goes through the argv args and create read stream for the data
 
-
-console.log('args', args)
-
-
-// It will read the file in chunks of the size which is specified before hand
-fs.createReadStream(`./data/${args[0]}`)
+const createReadStreamData = function(dataFile) {
+  fs.createReadStream(`./data/${dataFile}`)
   .pipe(csv())
   .on('data', (data) => console.log(data))
-  .on('end', () => {
-    console.log('End')
-  });
+}
 
-// It will read the file completely into memory before making it available for the user
-// fs.readFile(`./data/${args[0]}`, 'utf8', function (err, data) {
-//   console.log('data: ', data)
-// })
+const allData = args.forEach(createReadStreamData)
+
+
+// NTS--------------
+// fs.createReadStream: It will read the file in chunks of the size which is specified before hand
+// fs.readFile: It will read the file completely into memory before making it available for the user
+
 
