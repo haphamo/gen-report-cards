a5ec7740c25648a2e230edbba6ef1dafb0d67e27
haphamo <hapham0392@gmail.com>
2020-04-28 15:08:41 -0400

    Added sort feature to sort students by id

--- app.js
+++ app.js
@@ -2,18 +2,37 @@ const fs              = require('fs');
 const csv             = require('csv-parser')
 const args            = process.argv.slice(2)
 
-// create a function that goes through the argv args and create read stream for the data
-
-const createReadStreamData = function(dataFile) {
-  fs.createReadStream(`./data/${dataFile}`)
-  .pipe(csv())
-  .on('data', (data) => console.log(data))
+const rawStudentsData = []
+const rawMarksData = args[1]
+const rawTestsData = args[2]
+const rawCoursesData = args[3]
+// final json
+const result = {
+  students: []
 }
 
-const allData = args.forEach(createReadStreamData)
+fs.createReadStream(`./data/${args[1]}`)
+  .pipe(csv())
+  .on('data', (data) => {
+    result.students.push({id: parseInt(data.id), name: data.name})
+
+  })
+  .on('end', () => {
+    // sorts the students by id
+    result.students.sort(function(a, b) { 
+      return a.id - b.id;
+    });
+    console.log(result.students)
+
+  })
+
 
 
 // NTS--------------
+// JSON result is an obj with a students key (arr)
+// students.csv has a similar to the final result
+// use map to generate the students arr
+// node app.js courses.csv students.csv tests.csv marks.csv output.json
 // fs.createReadStream: It will read the file in chunks of the size which is specified before hand
 // fs.readFile: It will read the file completely into memory before making it available for the user
 
