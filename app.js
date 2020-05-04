04adf79e5fb88903b8b324b57d52fcc7b2be77dc
haphamo <hapham0392@gmail.com>
2020-05-04 16:10:25 -0400

    Created async functions. Working on gathering marks data for each student

--- app.js
+++ app.js
@@ -1,30 +1,87 @@
 const fs              = require('fs');
-const csv             = require('csv-parser')
-const args            = process.argv.slice(2)
+const fsp             = require('fs').promises;
+const csv             = require('csv-parser');
+const neatCsv         = require('neat-csv');
+const args            = process.argv.slice(2);
 
-const rawStudentsData = []
-const rawMarksData = args[1]
-const rawTestsData = args[2]
-const rawCoursesData = args[3]
-// final json
-const result = {
-  students: []
+// final JSON
+let result = {
+  students: [],
+  marks: []
 }
 
-fs.createReadStream(`./data/${args[1]}`)
-  .pipe(csv())
-  .on('data', (data) => {
-    result.students.push({id: parseInt(data.id), name: data.name})
+// console.log(args) // => [ 'courses.csv', 'students.csv', 'tests.csv', 'marks.csv', output.json ]
 
-  })
-  .on('end', () => {
-    // sorts the students by id
-    result.students.sort(function(a, b) { 
-      return a.id - b.id;
-    });
-    console.log(result.students)
+async function writeDataToOutput() {
+  try {
 
-  })
+    fs.createReadStream(`./data/${args[0]}`)
+    .pipe(csv())
+    .on('data', (row) => {
+      result.students.push({id: parseInt(row.id), name: row.name, totalAverage: 0, courses: []});
+
+      result.students.sort(function(a, b) { 
+            return a.id - b.id;
+          });
+    })
+    .on('end', () => {
+        fs.createWriteStream('data/output.json', 'utf8')
+        .write(JSON.stringify(result));
+    })
+  }
+  catch (err) {
+    console.error("Error!")
+  }
+}
+
+async function readMarks() {
+  try {
+    fs.createReadStream(`./data/${args[1]}`)
+    .pipe(csv())
+    .on('data', (row) => {
+      // console.log(Object.keys(row)[0])
+      
+      result.marks.push({test_id: parseInt(row.test_id), student_id: parseInt(row.student_id), mark: parseInt(row.mark)})
+
+      // console.log(`${Object.keys(row)[1]}`)
+    })
+    .on('end', () => {
+      console.log(result.students)
+      
+    })
+  }
+  catch(err) {
+    console.error(err)
+  }
+}
+
+
+writeDataToOutput()
+readMarks()
+
+
+
+
+// fs.createReadStream(`./data/${args[0]}`)
+//   .pipe(csv())
+
+// streamReadStudents.on('data', (row) => {
+
+//     // set up data structure for students, parsed SID to a number
+//     result.students.push({id: parseInt(row.id), name: row.name, totalAverage: 0, courses: []})
+
+ 
+// })
+
+// streamReadStudents.on('end', () => {
+//   // sorts the students by id
+//   result.students.sort(function(a, b) { 
+//     return a.id - b.id;
+//   });
+//   // path is the last arguement
+//   fs.createWriteStream('data/output.json', 'utf8')
+//   .write(JSON.stringify(result))
+// })
 
 
 
@@ -35,5 +92,3 @@ fs.createReadStream(`./data/${args[1]}`)
 // node app.js courses.csv students.csv tests.csv marks.csv output.json
 // fs.createReadStream: It will read the file in chunks of the size which is specified before hand
 // fs.readFile: It will read the file completely into memory before making it available for the user
-
-
