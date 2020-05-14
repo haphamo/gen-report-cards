550995b8eea4cd2c28c6f51239966fc183d6c58b
haphamo <hapham0392@gmail.com>
2020-05-14 15:03:16 -0400

    Created promises for 3 of the file reads and a async function which add course id and weight to each mark

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const csv = require("csv-parser");
 const args = process.argv.slice(2);
+const csvFast = require('fast-csv');
 const { addCourseWeightAndCourseId, readStudentDataAndSetUpFinalJsonStructure, readMarks, readTests, readCourses, generateJsonReportCardForAllStudents, checkSumOfAllCourseWeights } = require('./helperFunctions')
 
 // an obj with student id's as keys and the values are the students marks & avgs
@@ -14,6 +15,117 @@ const readCsvFiles = {
   allMarks: [],
   allTests: []
 }
+
+const readStudentsStream = fs.createReadStream('data/students.csv')
+
+const setUpFinalJsonWithStudents = function(stream) {
+  return new Promise(function(resolve, reject) {
+    const result = {students:[]}
+    stream.pipe(csv())
+    .on('data', row => {
+      // handles empty lines
+      if(Object.keys(row).length > 0) {
+      // added a trim on the row.name, just in case the names have space in them
+      result.students.push({id: parseInt(row.id), name: row.name.trim()})
+      }
+    })
+    .on('end', () => resolve(result))
+    .on('error', error => reject(new Error(`Error: ${error}`)))
+  })
+}
+
+// from the students ids in result, find all the test written by a student
+// add course id and weight for each mark
+const readAllMarks = function() {
+  return new Promise(function(resolve, reject) {
+    const allMarks = []
+    
+    fs.createReadStream('data/marks.csv')
+    .pipe(csv())
+    .on('data', row => {
+      
+      if(Object.keys(row).length > 0) {
+        // added a trim on the row.name, just in case the names have space in them
+        allMarks.push({test_id: parseInt(row.test_id), student_id: parseInt(row.student_id), mark: parseInt(row.mark)})
+      }
+    })
+    .on('end', () => resolve(allMarks))
+    .on('error', error => reject(new Error(`Error: ${error}`)))
+  })
+}
+
+const getAllTests = function() {
+  return new Promise(function(resolve, reject) {
+    const allTests = {}
+    fs.createReadStream('data/tests.csv')
+    .pipe(csv())
+    .on('data', row => {
+      if(Object.keys(row).length > 0) {
+        allTests[row.id] = {test_id: parseInt(row.id), course_id: parseInt(row.course_id), weight: parseInt(row.weight)}
+      }
+    })
+    .on('end', () => resolve(allTests))
+    .on('error', error => reject(new Error(`Error: ${error}`)))
+  })
+}
+
+const addCourseIdAndWeightToMarks = function(allMarks, allTests) {
+  allMarks.map(mark => {
+    mark.course_id = allTests[mark.test_id].course_id
+    mark.weight = allTests[mark.test_id].weight
+  })
+  return allMarks
+}
+
+async function test() {
+  const resultOne = await readAllMarks()
+  const resultTwo = await getAllTests()
+  const final = addCourseIdAndWeightToMarks(resultOne, resultTwo)
+  console.log(final)
+  const stringify = JSON.stringify(final)
+  fs.writeFile('data/output.json', stringify, err => {
+    if(err) {
+      console.error(err)
+    }
+    console.log("Finished!")
+  })
+}
+
+const fixture = [
+  { test_id: 1, student_id: 1, mark: 78, course_id: 1, weight: 10 },
+  { test_id: 2, student_id: 1, mark: 87, course_id: 1, weight: 40 },
+  { test_id: 3, student_id: 1, mark: 95, course_id: 1, weight: 50 },
+  { test_id: 4, student_id: 1, mark: 32, course_id: 2, weight: 40 },
+  { test_id: 5, student_id: 1, mark: 65, course_id: 2, weight: 60 },
+  { test_id: 6, student_id: 1, mark: 78, course_id: 3, weight: 90 },
+  { test_id: 7, student_id: 1, mark: 40, course_id: 3, weight: 10 },
+  { test_id: 1, student_id: 2, mark: 78, course_id: 1, weight: 10 },
+  { test_id: 2, student_id: 2, mark: 87, course_id: 1, weight: 40 },
+  { test_id: 3, student_id: 2, mark: 15, course_id: 1, weight: 50 },
+  { test_id: 6, student_id: 2, mark: 78, course_id: 3, weight: 90 },
+  { test_id: 7, student_id: 2, mark: 40, course_id: 3, weight: 10 },
+  { test_id: 1, student_id: 3, mark: 78, course_id: 1, weight: 10 },
+  { test_id: 2, student_id: 3, mark: 87, course_id: 1, weight: 40 },
+  { test_id: 3, student_id: 3, mark: 95, course_id: 1, weight: 50 },
+  { test_id: 4, student_id: 3, mark: 32, course_id: 2, weight: 40 },
+  { test_id: 5, student_id: 3, mark: 65, course_id: 2, weight: 60 },
+  { test_id: 6, student_id: 3, mark: 78, course_id: 3, weight: 90 },
+  { test_id: 7, student_id: 3, mark: 40, course_id: 3, weight: 10 }
+]
+
+
+test()
+// console.log(addCourseIdAndWeightToMarks(fix1, fix2))
+
+// setUpFinalJsonWithStudents(readStudentsStream).then(resolve => console.log(resolve))
+
+
+
+// getAllTests().then(res => console.log(res))
+// execute test read then pass result into allMarks
+
+
+/////////////////////////////////////////////////////
 
 async function getAllStudents(studentsCsv) {
   try {
@@ -91,4 +203,4 @@ async function getAllCoursesAndGenerateJson(coursesCsv) {
   }
 }
 
-getAllCoursesAndGenerateJson(args[0])
+// getAllCoursesAndGenerateJson(args[0])
