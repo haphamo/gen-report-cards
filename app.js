42bbace8d699e3d1f56bcb8f4338da8a5706078b
haphamo <hapham0392@gmail.com>
2020-05-17 23:53:57 -0400

    Calc total avg for each student" "

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const readAllStudentsAndSetUpFinalJson = function() {
       }
       // sort students by id
       result.students.sort((a, b) => a.id - b.id)
-      
     })
     .on('end', () => resolve(result))
     .on('error', error => reject(new Error(`Error: ${error}`)))
@@ -30,7 +29,6 @@ const readAllMarks = function() {
     fs.createReadStream('data/marks.csv')
     .pipe(csv())
     .on('data', row => {
-      
       if(Object.keys(row).length > 0) {
         // added a trim on the row.name, just in case the names have space in them
         allMarks.push({test_id: parseInt(row.test_id), student_id: parseInt(row.student_id), mark: parseInt(row.mark)})
@@ -84,27 +82,21 @@ const marksOfEachStudentByCourseId = function(marksWithCourseIdAndWeight) {
 };
 
 const calculateAllCourseAvgsForEveryStudent = function(objOfStudentsWithMarks, allCourses) {
-  
-  // receives the result from marksOfEachStudentByCourseId
-  // read course data and add teacher and name
   const allStudentsWithCourseAvgs = {}
   for(let [studentId, courses] of Object.entries(objOfStudentsWithMarks)) {
     for(let [course, grades] of Object.entries(courses)) {
       let courseAvg;
       // compares the course's number of tests to the tests array length and gives the students a course avg of 0 if they've missed a test
       (grades.length < allCourses[course].numberOfTests) ? courseAvg = 0 : courseAvg = grades.reduce((prev, curr) => prev + curr, 0);
-      
       const courseAveToTwoDecimal = parseFloat(courseAvg.toFixed(2))
       
       if(!allStudentsWithCourseAvgs[studentId]) {
-
         allStudentsWithCourseAvgs[studentId] = [{id: parseInt(allCourses[course].id), courseAverage: courseAveToTwoDecimal, name: allCourses[course].name, teacher: allCourses[course].teacher}]
       } else {
         allStudentsWithCourseAvgs[studentId].push({ id: parseInt(allCourses[course].id), courseAverage: courseAveToTwoDecimal, name: allCourses[course].name, teacher: allCourses[course].teacher, })
       } 
     }
   }
-  console.log(allStudentsWithCourseAvgs)
   return allStudentsWithCourseAvgs
 };
 
@@ -128,13 +120,98 @@ const calcNumberOfTestsPerCourse = function(allCourses, allTests) {
   return allCourses
 };
 
-(async function final() {
+const fixture = {
+  "students": [
+    {
+      "id": 1,
+      "name": "Jenny Nguy",
+      "courses": [
+        {
+          "id": 1,
+          "courseAverage": 90.1,
+          "name": "Biology",
+          "teacher": "Mr. D"
+        },
+        {
+          "id": 2,
+          "courseAverage": 51.8,
+          "name": "History",
+          "teacher": " Mrs. P"
+        },
+        {
+          "id": 3,
+          "courseAverage": 74.2,
+          "name": "Math",
+          "teacher": " Mrs. C"
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Tammie Chung",
+      "courses": [
+        {
+          "id": 1,
+          "courseAverage": 50.1,
+          "name": "Biology",
+          "teacher": "Mr. D"
+        },
+        {
+          "id": 3,
+          "courseAverage": 74.2,
+          "name": "Math",
+          "teacher": " Mrs. C"
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Tamires Lowande",
+      "courses": [
+        {
+          "id": 1,
+          "courseAverage": 90.1,
+          "name": "Biology",
+          "teacher": "Mr. D"
+        },
+        {
+          "id": 2,
+          "courseAverage": 51.8,
+          "name": "History",
+          "teacher": " Mrs. P"
+        },
+        {
+          "id": 3,
+          "courseAverage": 74.2,
+          "name": "Math",
+          "teacher": " Mrs. C"
+        }
+      ]
+    }
+  ]
+};
+
+
+(function calculateStudentAvg (data) {
+  data.students.map(student => {
+    const sumOfCourseAvgs = student.courses.reduce((prev, curr) => prev + curr.courseAverage, 0)
+    const numberOfEnrolledCourses = student.courses.length
+    const totalAvg = parseFloat((sumOfCourseAvgs / numberOfEnrolledCourses).toFixed(2))
+    student.totalAverage = totalAvg
+    // console.log(student)
+  })
+  console.log(data)
+})(fixture);
+
+// TO DO: Check sum of all course weights!
+
+(async function() {
   // reads data from students.csv and sets up final json object
   const allStudents = await readAllStudentsAndSetUpFinalJson();
   // array of mark data from marks.csv
   const allMarks = await readAllMarks(); 
   // obj with test id's as keys and the course & weight as its value, data taken from tests.csv
-  const allTests = await readAllTests(); 
+  const allTests = await readAllTests();
   // obj with course id as keys and the name and teacher as it's value, data from courses.csv
   const allCourses = await readAllCourses();
   // maps through all the tests to find out the number of tests for every course
@@ -145,11 +222,13 @@ const calcNumberOfTestsPerCourse = function(allCourses, allTests) {
   const organizedMarks = marksOfEachStudentByCourseId(marksWithCourseIdAndWeight);
   // course avgs are calculated for each student resulting in an object with the student id as keys and its value is an array of the courses enrolled with their course average
   const studentDataWithTheirCourseAvgs = calculateAllCourseAvgsForEveryStudent(organizedMarks, allCourses);
-  // TO DO: calculate their total avg!
+  
   allStudents.students.map(student => {
     student.courses = studentDataWithTheirCourseAvgs[student.id]
   });
+  // TO DO: calculate their total avg!
 
+  // console.log(studentDataWithTheirCourseAvgs)
   const stringified = JSON.stringify(allStudents);
 
   fs.writeFile('data/output.json', stringified, err => {
