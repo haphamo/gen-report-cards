2a9652e3d560ae299a919a10b4f3c3156d5caf8d
haphamo <hapham0392@gmail.com>
2020-05-22 16:09:12 -0400

    Added 2 more copies of test. Once IIFE was removed from app, the bug was fixed

---
+++ tests/test_addCourseIdAndWeightToMarks.js
@@ -0,0 +1,17 @@
+const { expect, assert } = require('chai');
+const { calculateStudentAvg } = require('../app.js');
+
+
+describe("addCourseAndWeightToMarks function", () => {
+
+  it("It should add course weight and id keys to the second arg", () => {
+      expect([1, 2, 3]).to.have.lengthOf(3)
+  });
+
+  it("It should add course weight and id keys to the second arg", () => {
+      expect([1, 2, 3]).to.have.lengthOf(3)
+  });
+  it("It should add course weight and id keys to the second arg", () => {
+      expect([1, 2, 3]).to.have.lengthOf(3)
+  });
+});
